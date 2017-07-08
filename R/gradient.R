a1821f5efd9903c4f7142bda8d6fcd0376ba938b
Alfonso Reyes <alfonso.r.reyes@gmail.com>
2017-07-08 11:19:18 -0500

    add test for function calc_temperature_gradient

---
+++ R/gradient.R
@@ -0,0 +1,14 @@
+
+#' Calculate the temperature gradient based on bottomhole and wellhead temperature
+#'
+#' @param bht bottomhole temperature, deg F
+#' @param wht wellhead temperature, deg F
+#' @depth.bh  depth at bottohole, feet
+#' @export
+calc_temperature_gradient <- function(bht, wht, depth.bh) {
+    stopifnot(depth.bh != 0)
+    if (bht == wht)
+        warning("bottomhole and wellhead temperatures are the same. \n
+                Gradient will be 0")
+    (bht - wht) / depth.bh
+}
