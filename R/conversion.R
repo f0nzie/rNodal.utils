8ec3c773273d3491bba6aad6bf24d0d42877378d
Alfonso Reyes <alfonso.r.reyes@gmail.com>
2017-07-08 00:20:58 -0500

    add oil specific gravity functions

--- R/conversion.R
+++ R/conversion.R
@@ -1,3 +1,21 @@
+#' Convert API to speciific gravity
+#'
+#' @param api gravity of oil or condensate in API degrees
+#' @export
+api_to_specific_gravity <- function(api) {
+  141.5 / (api + 131.5)
+}
+
+#' Convert Speciific Gravity of oil or condensate to API
+#'
+#' @param sg specific gravity of oil or condensate
+#' @export
+specific_gravity_to_api <- function(sg) {
+  (141.5 / sg) - 131.5
+}
+
+
+
 #' Take objects and create a list using their names
 #'
 #' This avoids retyping the name of the object as in a list
